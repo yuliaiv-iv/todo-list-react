d614107335f0337dbae611387799465ebb6562bd
Yulia Ivanova <yuliaiv.yulia@yandex.ru>
2020-10-07 21:22:55 +1000

    Updates

---
+++ precache-manifest.cab030b14d13656470788906194f4daa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5475f0b2dddacfdbb4f3161a531dbdc7",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "00783f08ab08a2cc2b1e",
+    "url": "/todo-list-react/static/css/main.876b92cf.chunk.css"
+  },
+  {
+    "revision": "f31d89ad4429c0a5897c",
+    "url": "/todo-list-react/static/js/2.0a5b052f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list-react/static/js/2.0a5b052f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00783f08ab08a2cc2b1e",
+    "url": "/todo-list-react/static/js/main.ccb96591.chunk.js"
+  },
+  {
+    "revision": "d2ce1cbae4c6bd42d20e",
+    "url": "/todo-list-react/static/js/runtime-main.dc5b2452.js"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/todo-list-react/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/todo-list-react/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "fbef17390eee3dc92db116f78de92fa4",
+    "url": "/todo-list-react/static/media/deleteicon.fbef1739.svg"
+  },
+  {
+    "revision": "50621d269ed59badaee844db796348d1",
+    "url": "/todo-list-react/static/media/duplicateicon.50621d26.svg"
+  },
+  {
+    "revision": "4cc2e7488843cd8c0b4b0550d0b72942",
+    "url": "/todo-list-react/static/media/editicon.4cc2e748.svg"
+  }
+]);
